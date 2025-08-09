c9098aaba0fce2bd96c2877165a27351fe27f724
Anirudh-MV <anirudhmv21@gmail.com>
2025-08-09 23:54:20 +0530

    Add linkedin URL

--- data/resume.ts
+++ data/resume.ts
@@ -18,6 +18,7 @@ export const resume = {
   },
   links: {
     github: "https://github.com/Anirudh-MV",
+    linkedin: "https://www.linkedin.com/in/anirudh-mv-3ba53879/",
     email: "mailto:contact@anirudhmv.in",
     phone: "+91 xxxxxxxxxxx",
     soundcloud: "https://soundcloud.com/anirudh-mv",
